bcda1999eec3fcce3cfa286193651e14a48eef2a
Joshua <Jdwalli2003@yahoo.com>
2022-07-13 21:15:01 -0400

    Started header component

---
+++ src/components/navigation/Header.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const Header: FunctionComponent = () => {
+  return (
+    <div className="bg-black rounded-2xl pt-5 pr-5 pb-4 pl-4 w-screen text-white ">
+        <li> Home </li>
+    </div>
+  );
+};
+
+export { Header };
